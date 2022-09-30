4b1b2d5e565398bcb98678da7832b9e79b2fa444
Zubin Guan <zubing2@nycstudents.net>
2022-09-29 20:53:09 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -19,53 +19,51 @@ public class Main {
 
         DecimalFormat formatter = new DecimalFormat("#.##"); //Formatter to two decimal spots
 
+
         double input = 0; //All the input values starts as zeros and are assigned later.
         double total = 1; //Value of the total has to be 1 instead of 0 since the last input is -1.
-        double finalTotal; //Assigned double to each of variable
-        double finalTip;
-        double finalCostPerPerson;
-        double finalTipPerPerson;
-        double finalTotalWithTip;
+
 
         while (input != -1) { //While loop to ask for price of each item.
-            System.out.print("What is the cost of the item? (Type -1 to end): ");
+            System.out.print("What is the cost of the item? In Dollars and Cents (Type -1 to end): ");
             input = scan.nextDouble();
             total = (total + input); //Adds each input to the total
         }
-        //STD=String to Double
+
+
         System.out.println("----------------------------");
 
         String formattedNum = formatter.format(total); //Decimal Formatting for the initial cost
-        finalTotal = Double.parseDouble(formattedNum); //STD
+        double finalTotal = Double.parseDouble(formattedNum);
         System.out.println("The total cost before tax: " + finalTotal);
 
         System.out.println("Tip Percentage: " + tipAmount);
 
         double totalTip = (finalTotal * tipPercent); //Finding tip amt by multiplying total w/ adjusted tip
         String formattedTip = formatter.format(totalTip);
-        finalTip = Double.parseDouble(formattedTip); //STD
+        double finalTip = Double.parseDouble(formattedTip);
         System.out.println("Total Tip: " + finalTip);
 
         double totalWithTip = (finalTotal + finalTip); //Finding tip amt by multiplying total w/ adjusted tip
         String formattedTotalWithTip = formatter.format(totalWithTip);
-        finalTotalWithTip = Double.parseDouble(formattedTotalWithTip); //STD
+        double finalTotalWithTip = Double.parseDouble(formattedTotalWithTip);
         System.out.println("Total Bill with Tip: " + finalTotalWithTip);
 
         double costPerPerson = (finalTotal / groupAmount); //Finding cost per person
         String formattedCostPerPerson = formatter.format(costPerPerson);
-        finalCostPerPerson = Double.parseDouble(formattedCostPerPerson); //STD
+        double finalCostPerPerson = Double.parseDouble(formattedCostPerPerson);
         System.out.println("Cost per person before tip: " + (finalCostPerPerson));
 
         double tipPerPerson = (finalTip / groupAmount); //Tip per Person
         String formattedTipPerPerson = formatter.format(tipPerPerson);
-        finalTipPerPerson = Double.parseDouble(formattedTipPerPerson);//STD
+        double finalTipPerPerson = Double.parseDouble(formattedTipPerPerson);
         System.out.println("Tip per person: " + (finalTipPerPerson));
 
         double finalCostPerPersonWithTip = (finalCostPerPerson + finalTipPerPerson); //Final Cost Per Person w/ Tip
         String formattedFinalCostPerPerson = formatter.format(finalCostPerPersonWithTip);
-        finalCostPerPersonWithTip = Double.parseDouble(formattedFinalCostPerPerson);//STD
+        finalCostPerPersonWithTip = Double.parseDouble(formattedFinalCostPerPerson);
+        System.out.println("Total amount per person: " + finalCostPerPersonWithTip);
 
-        System.out.println("Total amount per person: " + (finalCostPerPersonWithTip));
         System.out.print("----------------------------");
     }
 }
